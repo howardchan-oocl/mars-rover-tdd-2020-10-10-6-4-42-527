db165712946a2e48667e6d4448024f1ecb72eb99
chanho <howard.chan@oocl.com>
2020-11-26 17:16:44 +0800

    [Mars Rover][Howard Chan]implement the test case and the requirement of given N and M

---
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -0,0 +1,40 @@
+package com.afs.tdd;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MarsRover {
+    private int locationX;
+    private int locationY;
+    private String direction;
+
+    public MarsRover(int locationX, int locationY, String direction) {
+
+        this.locationX = locationX;
+        this.locationY = locationY;
+        this.direction = direction;
+    }
+
+    public void executeCommands(String commands) {
+        new ArrayList<>(Arrays.asList(commands.split("")))
+                .forEach(this::move);
+    }
+
+    private void move(String command) {
+        if (command.equals("M") && direction.equals("N")) {
+            locationY += 1;
+        }
+    }
+
+    public int getLocationX() {
+        return locationX;
+    }
+
+    public int getLocationY() {
+        return locationY;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+}
